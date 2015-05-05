4f2735b37260bbb4ee9bc1562157edd54873270c
Balz Caflisch <Balz@Balzs-MacBook-Pro.local>
2015-05-05 17:51:27 +0200

    commit

--- src/cov/SquaredExponential.java
+++ src/cov/SquaredExponential.java
@@ -1,6 +1,16 @@
 package cov;
 import org.jblas.DoubleMatrix;
 public class SquaredExponential extends CovarianceFunction{
-	public void train(){}
-	public DoubleMatrix computeCovMatrix(DoubleMatrix k, DoubleMatrix kstar){return k;}
+	public DoubleMatrix parameters;
+	public double computeCovariance(double x, double xstar, DoubleMatrix parameters){
+		this.parameters = parameters;
+		double d = x-xstar;
+		return parameters.get(0)*parameters.get(0) *Math.exp(- 1/(2 * parameters.get(1)*parameters.get(1)) * d*d);
+		
+	}
+	public double computeCovariance(DoubleMatrix x, DoubleMatrix xstar, DoubleMatrix parameters){
+		this.parameters = parameters;
+		double d = x.distance2(xstar);
+		return parameters.get(0)*parameters.get(0) *Math.exp(- 1/(2 * parameters.get(1)*parameters.get(1)) * d*d);
+	}
 }
